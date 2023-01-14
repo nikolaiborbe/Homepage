384fe08ad1fb20264cd5ab236593b79aa6141b5f
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-01-14 19:34:38 +0100

    I don't know what I'm doing

---
+++ src/components/NavbarMobile.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Logo from '../static/NGB-logo.svg'
+  import Hamburger from "./Hamburger.svelte";
+
+  let menu = false
+  function handleClick(event) {
+    menu = true
+  }
+</script>
+
+<div class="navbar bg-base-100 flex justify-between px-6">
+  <a href="/"><img src={Logo} alt="" class="w-14"></a>
+  <button on:click={handleClick}>
+    <Hamburger />
+  </button>
+  {#if menu == true}
+    <div>
+      TEST
+    </div>
+  {/if}
+</div>
+
+<style>
+  .navbar {
+    border-bottom: thin solid rgb(66, 66, 66);
+  }
+</style>
