4bbdf1da548cdb7aa1c15e58a8034372ed29a172
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-01-20 23:07:59 +0100

    don't know how to do this

--- src/components/Subjects.svelte
+++ src/components/Subjects.svelte
@@ -5,17 +5,33 @@
     export let subjects;
     export let chapters;
 
-
     let searchTerm = "";
-    $: searchedPosts = posts.filter((post) => {
-      let out = post.frontmatter.title.toLowerCase().includes(searchTerm.toLowerCase()) +
-      post.frontmatter.chapter.toLowerCase().includes(searchTerm.toLowerCase()) +
-      post.frontmatter.subject.toLowerCase().includes(searchTerm.toLowerCase())
-      return out
-    } )
+    let select_subject;
+    let select_chapter;
+    let subject_search = "";
+    let chapter_search = "";
+    let search = "";
 
-    let selected_chapter;
-    let selected_subject;
+    $: if (subjects.includes(select_subject)) {
+      subject_search = `${select_subject}`
+    } else {
+      subject_search = "";
+    }
+    $: if (chapters.includes(select_chapter)) {
+      chapter_search = `${select_chapter}`
+    } else {
+      chapter_search = "";
+    }
+
+
+    $: searchedPosts = posts.filter((post) => {
+      // let s_title = post.frontmatter.title.toLowerCase().includes(searchTerm.toLowerCase().trim())
+      let s_chapters = post.frontmatter.chapter.toLowerCase().includes(chapter_search.toLowerCase().trim())
+      let s_subjects = post.frontmatter.subject.toLowerCase().includes(subject_search.toLowerCase().trim())
+      // let out = [s_title, s_chapters, s_subjects]
+      console.log(s_subjects)
+      return s_subjects
+    })
 
 </script>
 
@@ -30,15 +46,17 @@
   <div class="grid grid-cols-2 gap-5 justify-center max-sm:px-5">
 
     <div class="max-sm:hidden">
-      <select bind:value={selected_subject} class="bg-black select select-bordered w-full">
+
+      <select bind:value={select_subject} class="bg-black select select-bordered w-full">
         <option selected class="text-zinc-500">Select subject</option>
         {#each subjects as subject}
           <option value={subject}>{subject}</option>
         {/each}
       </select>
+
     </div>
     <div class="sm:hidden">
-      <select bind:value={selected_subject} class="bg-black">
+      <select bind:value={select_subject} class="bg-black">
         <option selected class="text-zinc-500">Select subject</option>
         {#each subjects as subject}
           <option value={subject}>{subject}</option>
@@ -47,7 +65,7 @@
     </div>
   
     <div class="max-sm:hidden">
-      <select bind:value={selected_chapter} class="bg-black select select-bordered w-full">
+      <select bind:value={select_chapter} class="bg-black select select-bordered w-full">
         <option selected class="text-zinc-500">Select chapter</option>
         {#each chapters as chapter}
           <option value={chapter}>{chapter}</option>
@@ -56,7 +74,7 @@
     </div>
     
     <div class="sm:hidden">
-      <select bind:value={selected_chapter} class="bg-black">
+      <select bind:value={select_chapter} class="bg-black">
         <option selected class="text-zinc-500">Select chapter</option>
         {#each chapters as chapter}
           <option value={chapter}>{chapter}</option>
