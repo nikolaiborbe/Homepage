50b905b02d640922940f21889747733a32057a28
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-01-21 18:49:16 +0100

    smol nomber shaky

--- src/components/IndexTitle.svelte
+++ src/components/IndexTitle.svelte
@@ -13,7 +13,7 @@
     <div class="m-auto max-sm:px-4">
         <NGB />
         <h3 class="text-gray-400 text-xl font-light pt-12 pb-32 text-center lg:px-48 px-10">
-            I'm a <span class="font-bold inline-block w-36">
+            I'm a <span class="font-bold inline-block w-36 px-1">
                 <Age client:load b_date={b_date}/>
             </span> year-old <span>
                 <Roulette client:load items={personal} />
