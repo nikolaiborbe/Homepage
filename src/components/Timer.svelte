ee44a59bc69c8ebaeaa862d8c1b7e5cf60618667
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-01-22 17:33:27 +0100

    yes

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -1,6 +1,6 @@
 <script>
     import { timeUntil } from "../lib/func";
-    import OnInterval from "./onInterval.svelte";
+    import OnInterval from "./OnInterval.svelte";
 
     let date = "08/20/2024";
     let time_until_school = timeUntil(date);
