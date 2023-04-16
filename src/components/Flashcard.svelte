031faba1891d4be91862b3e98fceaca350f749c3
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-04-16 17:28:48 +0200

    test?

--- src/components/Flashcard.svelte
+++ src/components/Flashcard.svelte
@@ -7,13 +7,13 @@
   <label class="swap swap-flip text-xl">
     <input type="checkbox" autocomplete="off"/>
 
-    <div class="swap-off w-96 h-64 bg-[#0d1117] rounded-3xl">
+    <div class="swap-off w-[22rem] h-64 bg-[#0d1117] rounded-3xl">
       <div class="h-full w-full flex text-center">
         <span class="text-zinc-500 text-base p-3 pl-5 absolute">Question</span>
         <p class="w-full justify-self-center self-center px-5">{q}</p>
       </div>
     </div>
-    <div class="swap-on w-96 h-64 bg-[#0d1117] rounded-3xl">
+    <div class="swap-on w-[22rem] h-64 bg-[#0d1117] rounded-3xl">
       <div class="h-full w-full flex text-center">
         <span class="text-zinc-500 text-base p-3 pl-5 absolute">Answer</span>
         <p class="w-full justify-self-center self-center px-5">{a}</p>
