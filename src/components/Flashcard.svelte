3948dc58a8a83ffaa55b1fdf99aeee607fe2c40f
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-04-16 17:34:19 +0200

    I was wrong

--- src/components/Flashcard.svelte
+++ src/components/Flashcard.svelte
@@ -1,19 +1,19 @@
 <script>
-    export let q;
-    export let a;
+  export let q;
+  export let a;
 </script>
 
 <div class="flex justify-center">
   <label class="swap swap-flip text-xl">
-    <input type="checkbox" autocomplete="off"/>
+    <input type="checkbox" autocomplete="off" />
 
-    <div class="swap-off w-96 h-64 bg-[#0d1117] rounded-3xl">
+    <div class="swap-off w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
       <div class="h-full w-full flex text-center">
         <span class="text-zinc-500 text-base p-3 pl-5 absolute">Question</span>
         <p class="w-full justify-self-center self-center px-5">{q}</p>
       </div>
     </div>
-    <div class="swap-on w-96 h-64 bg-[#0d1117] rounded-3xl">
+    <div class="swap-on w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
       <div class="h-full w-full flex text-center">
         <span class="text-zinc-500 text-base p-3 pl-5 absolute">Answer</span>
         <p class="w-full justify-self-center self-center px-5">{a}</p>
