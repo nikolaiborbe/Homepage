f90ef8a1f93e4465f7b486b03376d11ee3f649f3
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2023-04-25 18:43:23 +0200

    new flashcard

--- src/components/Flashcard.svelte
+++ src/components/Flashcard.svelte
@@ -3,21 +3,23 @@
   export let a;
 </script>
 
-<div class="flex justify-center">
-  <label class="swap swap-flip text-xl">
-    <input type="checkbox" autocomplete="off" />
+<div class="flex flex-wrap justify-center">
+  <div class="flex justify-center">
+    <label class="swap swap-flip text-xl">
+      <input type="checkbox" autocomplete="off" />
 
-    <div class="swap-off w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
-      <div class="h-full w-full flex text-center">
-        <span class="text-zinc-500 text-base p-3 pl-5 absolute">Question</span>
-        <p class="w-full justify-self-center self-center px-5">{q}</p>
+      <div class="swap-off w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
+        <div class="h-full w-full flex text-center">
+          <span class="text-zinc-500 text-base p-3 pl-5 absolute">Question</span>
+          <p class="w-full justify-self-center self-center px-5">{q}</p>
+        </div>
       </div>
-    </div>
-    <div class="swap-on w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
-      <div class="h-full w-full flex text-center">
-        <span class="text-zinc-500 text-base p-3 pl-5 absolute">Answer</span>
-        <p class="w-full justify-self-center self-center px-5">{a}</p>
+      <div class="swap-on w-[23rem] h-64 bg-[#0d1117] rounded-3xl">
+        <div class="h-full w-full flex text-center">
+          <span class="text-zinc-500 text-base p-3 pl-5 absolute">Answer</span>
+          <p class="w-full justify-self-center self-center px-5">{a}</p>
+        </div>
       </div>
-    </div>
-  </label>
+    </label>
+  </div>
 </div>
