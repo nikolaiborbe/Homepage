16dd81299b1be6d150a8babdace0b6e1c9c858d0
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-11-17 12:16:08 +0100

    simplify website, add more books, remove blog & class-notes

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,8 +1,6 @@
 <div class="h-screen w-full bg-black flex flex-col ">
     <ul class="text-xl font-thin px-5 pt-28">
         <a href="/portfolio"><li class="border border-t-0 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">👨‍🚀 Portfolio</li></a>
-        <a href="/class-notes"><li class="border border-t-1 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">📖 Class Notes</li></a>
-        <a href="/blog"><li class="border border-t-1 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">✍️ Blog</li></a>
         <a href="/books"><li class="border border-t-1 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">📕 Books</li></a>
         <a href="/about"><li class="border border-t-1 border-b-1 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">🙋 About</li></a>
     </ul>
