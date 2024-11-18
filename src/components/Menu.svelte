faffa821ee19997aedf1bb5fd0ae83dfd3ba413b
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-11-18 20:47:06 +0100

    add blog link

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,6 +1,7 @@
 <div class="h-screen w-full bg-black flex flex-col ">
     <ul class="text-xl font-thin px-5 pt-28">
         <a href="/portfolio"><li class="border border-t-0 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">👨‍🚀 Portfolio</li></a>
+        <a href="https://blog-nikolai.netlify.app" target="_blank"><li class="border border-t-1 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">✍️ Blog</li></a>
         <a href="/books"><li class="border border-t-1 border-b-0 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">📕 Books</li></a>
         <a href="/about"><li class="border border-t-1 border-b-1 border-x-0 py-4 pl-2 hover:bg-zinc-800 rounded-sm">🙋 About</li></a>
     </ul>
